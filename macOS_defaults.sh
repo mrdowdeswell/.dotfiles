914081699fa3836b41e7a4bad307408b24daa471
Mark Dowdeswell <123539509+mrdowdeswell@users.noreply.github.com>
2026-05-28 15:26:29 +0200

    Fix trackpad tap to click setting in macOS defaults script

--- macOS_defaults.sh
+++ macOS_defaults.sh
@@ -18,7 +18,7 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 25
 
 # ── Trackpad / Mouse ──────────────────────────────────────────────────────────
 # Tap to click
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # ── Finder ────────────────────────────────────────────────────────────────────
